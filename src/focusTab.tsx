86fb474f024501b3a629fd09c77d388b60b82302
Nihal Sheikh <nihalsheikh960@gmail.com>
2024-05-02 20:21:39 +0600

    Made pause feature functional

--- src/focusTab.tsx
+++ src/focusTab.tsx
@@ -1,15 +1,27 @@
 import PomodoroInput from "./PomodoroInput";
 
 export default function focusMode() {
-    return <>
-                <section>
-                    <h1 className="header">This is the podomoro timer focus mode</h1>
-                    <p className="description">The pomodoro timer is a productivity technique that helps you focus on a task for a fixed amount of time, usually 25 minutes, followed by a short break, usually 5 minutes. The idea is to break down your work into manageable chunks and avoid distractions. The pomodoro timer can be a simple kitchen timer, a smartphone app, or a web-based tool. The benefits of using the pomodoro timer include increased concentration, reduced stress, and improved time management.
-                    </p><br />
-                    <p className="description">Start using pomodoro with persistant by clicking the button below. Set rest time to 0 for no rest(not recomended)</p>
-                </section>
-                <section className="pomodoro">                        
-                  <PomodoroInput/>
-                </section>
-        </>
+  return (
+    <>
+      <section>
+        <h1 className="header">This is the podomoro timer focus mode</h1>
+        <p className="description">
+          The pomodoro timer is a productivity technique that helps you focus on
+          a task for a fixed amount of time, usually 25 minutes, followed by a
+          short break, usually 5 minutes. The idea is to break down your work
+          into manageable chunks and avoid distractions. The pomodoro timer can
+          be a simple kitchen timer, a smartphone app, or a web-based tool. The
+          benefits of using the pomodoro timer include increased concentration,
+          reduced stress, and improved time management.
+        </p>
+        <p className="description">
+          Start using pomodoro with persistant by clicking the button below. Set
+          rest time to 0 for no rest(not recomended)
+        </p>
+      </section>
+      <section className="pomodoro">
+        <PomodoroInput />
+      </section>
+    </>
+  );
 }
--- src/pomodoro.tsx
+++ src/pomodoro.tsx
@@ -1,29 +1,52 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from "react";
 interface AppProps {
   restTime: number;
   sessionTime: number;
   repeatCount: number;
 }
 export default function App(props: AppProps) {
-    const restTime: number = props.restTime * 60;
-    const workTime: number = props.sessionTime * 60;
-    const repeats: number = (props.repeatCount * 2);
-    const totalTime = ((props.sessionTime) + (props.restTime)) * (props.repeatCount)
-    const [repeatsDone, setRepeatsDone] = useState<number>(0);
-    const [totalSeconds, setTotalSeconds] = useState<number>(0);
-    const [currentSession, setCurrentSession] = useState<number>(workTime);
-    const [working, setWorking] = useState<boolean>(true);
-    const [Paused, setPaused] = useState<boolean>(false);
-    let date: Date;
-    useEffect(() => {
-      date = new Date();
-      const timeInSeconds: number = (date.getHours() * 3600) + (date.getMinutes() * 60) + date.getSeconds();
-      const interval: NodeJS.Timeout = setInterval(() => {
-        setTotalSeconds(() => {
+  const restTime: number = props.restTime * 60;
+  const workTime: number = props.sessionTime * 60;
+  const repeats: number = props.repeatCount * 2;
+  const totalTime = (props.sessionTime + props.restTime) * props.repeatCount;
+  const [repeatsDone, setRepeatsDone] = useState<number>(0);
+  const [totalSeconds, setTotalSeconds] = useState<number>(0);
+  const [currentSession, setCurrentSession] = useState<number>(workTime);
+  const [working, setWorking] = useState<boolean>(true);
+  const Paused = useRef<boolean>(false);
+  const pauseTimeinSeconds = useRef<number>(0);
+  const resumeTimeinSeconds = useRef<number>(0);
+  const totalPauseTimeinSeconds = useRef<number>(0);
+  const modifier = useRef<number>(1);
+  let date: Date;
+  useEffect(() => {
+    if (totalTime === 0) {
+      return;
+    }
+    date = new Date();
+    const timeInSeconds: number =
+      date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
+    const interval: NodeJS.Timeout = setInterval(() => {
+      setTotalSeconds(() => {
+        if (Paused.current) {
+          const d: Date = new Date();
+          resumeTimeinSeconds.current =
+            d.getHours() * 3600 +
+            d.getMinutes() * 60 +
+            d.getSeconds() +
+            modifier.current;
+          totalPauseTimeinSeconds.current =
+            resumeTimeinSeconds.current - pauseTimeinSeconds.current;
+        }
+
         const newTime = new Date();
-        const newSeconds: number = (newTime.getHours() * 3600) + (newTime.getMinutes() * 60) + newTime.getSeconds();
-        const newTotalSeconds: number = newSeconds - timeInSeconds;
-        console.log(newTotalSeconds,timeInSeconds,newSeconds);
+        const newSeconds: number =
+          newTime.getHours() * 3600 +
+          newTime.getMinutes() * 60 +
+          newTime.getSeconds();
+        const newTotalSeconds: number =
+          newSeconds - (timeInSeconds + totalPauseTimeinSeconds.current);
+
         if (newTotalSeconds >= currentSession) {
           if (working) {
             setCurrentSession(restTime);
@@ -37,46 +60,61 @@ export default function App(props: AppProps) {
       });
     }, 1000);
     if (repeatsDone >= repeats) {
-      clearInterval(interval)
-    }
-    
-      return () => clearInterval(interval);
-    }, [repeatsDone]);
-  
-    useEffect(() => {
-      if (repeatsDone < repeats) {
-        setTotalSeconds(0);
-        setRepeatsDone((prevRepeatsDone) => prevRepeatsDone + 1);
-      }
-    }, [working, currentSession]);
-    useEffect(() => {
-      setTotalSeconds(0);
-      setRepeatsDone(0);
-      setWorking(true);
-      setCurrentSession(workTime);
-    }, [props.sessionTime, props.restTime, props.repeatCount]);
-  
-    const seconds: number = totalSeconds % 60;
-    const minutes: number = Math.floor(totalSeconds / 60) % 60;
-    const hours: number = Math.floor(totalSeconds / 3600);
-    if (props.sessionTime === 0) {
-        return <></>;
-    } else if (totalTime > 480) {
-        window.alert('Total time should be less than 8 hours. We do not allow nor encourage overtime');
-        return <></>;
+      clearInterval(interval);
     }
 
-    return (
-        <div>
-          <h1 className='clock'>
-              {hours > 0 ? hours : ''}
-              {hours > 0 ? ':' : ''}
-              {minutes > 9 ? '' : '0'}
-              {minutes}:{seconds > 9 ? '' : '0'}
-              {seconds}
-              <sup className='session'>{working ? 'Work Session' : 'Rest Session'}</sup>
-          </h1>
-          <button>{Paused ? 'Unpause' : 'Pause'}</button>
-        </div>
+    return () => clearInterval(interval);
+  }, [repeatsDone]);
+  useEffect(() => {
+    if (repeatsDone < repeats) {
+      setTotalSeconds(0);
+      setRepeatsDone((prevRepeatsDone) => prevRepeatsDone + 1);
+    }
+  }, [working, currentSession]);
+  useEffect(() => {
+    setTotalSeconds(0);
+    setRepeatsDone(0);
+    setWorking(true);
+    setCurrentSession(workTime);
+  }, [props.sessionTime, props.restTime, props.repeatCount]);
+  function handlePause() {
+    Paused.current = !Paused.current;
+    if (Paused.current) {
+      const d = new Date();
+      pauseTimeinSeconds.current =
+        d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
+    } else {
+      modifier.current =
+        resumeTimeinSeconds.current - pauseTimeinSeconds.current;
+    }
+  }
+  const seconds: number = totalSeconds % 60;
+  const minutes: number = Math.floor(totalSeconds / 60) % 60;
+  const hours: number = Math.floor(totalSeconds / 3600);
+  if (props.sessionTime === 0) {
+    return <></>;
+  } else if (totalTime > 480) {
+    window.alert(
+      "Total time should be less than 8 hours. We do not allow nor encourage overtime"
     );
+    return <></>;
+  }
+
+  return (
+    <div className="clockContainer">
+      <h1 className="clock">
+        {hours > 0 ? hours : ""}
+        {hours > 0 ? ":" : ""}
+        {minutes > 9 ? "" : "0"}
+        {minutes}:{seconds > 9 ? "" : "0"}
+        {seconds}
+        <sup className="session">
+          {working ? "Work Session" : "Rest Session"}
+        </sup>
+      </h1>
+      <button type="button" onClick={() => handlePause()} className="pause">
+        {Paused.current ? "Unpause" : "Pause"}
+      </button>
+    </div>
+  );
 }
