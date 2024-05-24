c7769128a0b392bbc386bdb7cd1f11c190937bb4
Nihal Sheikh <nihalsheikh960@gmail.com>
2024-05-24 12:32:52 +0600

    server init

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const Express = require("express");
-const app = Express();
-
-app.get("/api", (req, res) => {
-    res.send("Hello World! I am a fullstack developer");
-});
-
-app.listen(3000, () => console.log("Server started on port 3000"));
---
+++ server/index.ts
@@ -0,0 +1,8 @@
+import Express from "express";
+const app = Express();
+
+app.get("/api", (req, res) => {
+  res.send("Hello World! I am a fullstack developer");
+});
+
+app.listen(3000, () => console.log("Server started on port 3000"));
